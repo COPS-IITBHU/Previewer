0050cc3b82986c48d3bebe16d1a828a3f016e8b6
puranjayK <puranjay01@gmail.com>
2021-10-17 20:51:11 +0530

    Added support for generating video thumbnail

--- lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
+++ lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
@@ -1,5 +1,9 @@
 package com.cops.iitbhu.previewer.lib
 
+import android.net.Uri
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
 class Previewer {
     companion object{
         private var instance : Previewer? = null
@@ -12,5 +16,11 @@ class Previewer {
 
     fun printHello() {
         println("Hello World")
+    }
+    fun getVideoThumbnail(uri: Uri, imageView: ImageView)  {
+        Glide.with(imageView)
+            .asBitmap()
+            .load(uri)
+            .into(imageView)
     }
 }
