31403ab652058eeef7d9b29ed7604325a57567f1
puranjayK <puranjay01@gmail.com>
2021-10-19 21:12:20 +0530

    Refactored Previewer.kt into an object

--- lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
+++ lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
@@ -2,17 +2,12 @@ package com.cops.iitbhu.previewer.lib
 
 import android.app.Application
 
-class Previewer {
-    companion object{
-        private var instance : Previewer? = null
-        private var contextApplication : Application? = null
-        fun getInstance() : Previewer {
-            if(instance==null) instance = Previewer()
-            return instance!!
-        }
-        fun init(contextApplication: Application) {
-            this.contextApplication=contextApplication
-        }
+object Previewer{
+
+    private var contextApplication : Application? = null
+
+    fun init(contextApplication: Application) {
+        this.contextApplication=contextApplication
     }
 
 }
