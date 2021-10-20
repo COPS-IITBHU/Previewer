0bb10a7c05f57f64ec03216d3ed68c8dec08c188
puranjayK <puranjay01@gmail.com>
2021-10-20 11:08:06 +0530

    Removed unused imports

--- app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
+++ app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.cops.iitbhu.previewer
 
-import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.cops.iitbhu.previewer.lib.Previewer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
