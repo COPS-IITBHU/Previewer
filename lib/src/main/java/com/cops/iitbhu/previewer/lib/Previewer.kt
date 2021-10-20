3093144f685123ef62189c1a30413d7bf29058f8
puranjayK <puranjay01@gmail.com>
2021-10-20 14:04:37 +0530

    added necessary imports

--- lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
+++ lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
@@ -1,15 +1,20 @@
 package com.cops.iitbhu.previewer.lib
 
+import android.app.Application
+import android.net.Uri
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
 object Previewer {
 
     private lateinit var appContext: Application
 
     fun init(appContext: Application) {
         this.appContext = appContext
-      
+
     }
-     
-    fun getVideoThumbnail(uri: Uri, imageView: ImageView)  {
+
+    fun getVideoThumbnail(uri: Uri, imageView: ImageView) {
         Glide.with(imageView)
             .asBitmap()
             .load(uri)
