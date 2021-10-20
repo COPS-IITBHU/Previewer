f73c1367de50cd85d34adb63e090860eee75e436
ayushigupta931 <ayushigupta931@gmail.com>
2021-10-20 15:17:23 +0530

    Bitmap of PDF

--- app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
+++ app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
@@ -1,11 +1,117 @@
 package com.cops.iitbhu.previewer
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.cops.iitbhu.previewer.lib.Previewer
 
 class MainActivity : AppCompatActivity() {
+
+    private val READ_EXTERNAL_STORAGE_PERMISSION_CODE = 101
+    private lateinit var img: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectFile: Button = findViewById(R.id.select_file)
+        val youtubeLink: EditText =findViewById(R.id.youtube_link)
+        val generateYouTubeThumbnail : Button = findViewById(R.id.generate_thumbnail_for_youtube)
+        img = findViewById(R.id.img)
+
+        selectFile.setOnClickListener(View.OnClickListener {
+
+            checkPermissions(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                READ_EXTERNAL_STORAGE_PERMISSION_CODE
+            )
+        })
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+    private val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+            if (result.resultCode == Activity.RESULT_OK) {
+
+                val intent = result.data
+                val uri = intent?.data
+
+                if (uri != null) {
+                    val bitmap = Previewer.generateBitmap(uri)
+                    if (bitmap != null)
+                        img.setImageBitmap(bitmap)
+                    else
+                        Toast.makeText(this,"Unable to generate bitmap!",Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+
+    //To get URI of pdf
+    private fun getUri() {
+        val getUriOfFile = Intent().apply {
+            action = Intent.ACTION_GET_CONTENT
+            type = "application/pdf"
+            addCategory(Intent.CATEGORY_OPENABLE)
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false)
+        }
+
+        try {
+            startForResult.launch(getUriOfFile)
+        }
+        catch (e: ActivityNotFoundException) {
+            println(e)
+        }
+    }
+
+
+    private fun checkPermissions(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, "Permission already granted!", Toast.LENGTH_SHORT).show()
+            getUri()
+        }
+        else {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == READ_EXTERNAL_STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show()
+                getUri()
+            } else {
+                Toast.makeText(this, "Permission denied!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
+++ lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
@@ -1,6 +1,17 @@
 package com.cops.iitbhu.previewer.lib
 
 import android.app.Application
+import android.graphics.Bitmap
+import android.graphics.pdf.PdfRenderer
+import android.net.Uri
+import android.os.Environment.DIRECTORY_PICTURES
+import android.os.ParcelFileDescriptor
+import java.io.File
+import java.io.FileOutputStream
+import android.util.Log
+import android.widget.ImageView
+
+import java.net.MalformedURLException
 
 object Previewer {
 
@@ -9,5 +20,69 @@ object Previewer {
     fun init(appContext: Application) {
         this.appContext = appContext
     }
+
+    //Generates bitmap for pdf
+    fun generateBitmap(uri: Uri) : Bitmap?{
+
+        try {
+            val fileName = "@${System.currentTimeMillis()}"
+            val storageDir = appContext.getExternalFilesDir(DIRECTORY_PICTURES)
+            val file = File.createTempFile(fileName, ".pdf", storageDir!!)
+            val inputStream = appContext.contentResolver.openInputStream(uri)!!
+            val outputStream = FileOutputStream(file)
+            val buffer = ByteArray(1024)
+            var len: Int
+
+            while (inputStream.read(buffer).also { len = it } >= 0) {
+                outputStream.write(buffer, 0, len)
+            }
+
+            outputStream.close()
+            inputStream.close()
+
+            val fileDescriptor = ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
+            val pdfRenderer = PdfRenderer(fileDescriptor)
+            val page = pdfRenderer.openPage(0)
+            var bitmap: Bitmap = Bitmap.createBitmap(
+                page.width,
+                page.height,
+                Bitmap.Config.ARGB_8888
+            )
+
+            page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+            bitmap = Bitmap.createBitmap(bitmap,0,0,page.width,(page.height)/3)
+
+            page.close()
+            pdfRenderer.close()
+            fileDescriptor.close()
+            return bitmap
+        }
+        catch (e : Exception){
+            println(e)
+            return null
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
