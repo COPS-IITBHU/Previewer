93be9e06c1ec44e0789c252c202bf62da0100a93
RainaJain5 <rj35100@gmail.com>
2021-10-20 15:36:53 +0530

    Youtube video thumbnail

--- app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
+++ app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
@@ -1,11 +1,117 @@
 package com.cops.iitbhu.previewer
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.cops.iitbhu.previewer.lib.Previewer
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var img: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val selectFile: Button = findViewById(R.id.select_file)
+        val youtubeLink: EditText =findViewById(R.id.youtube_link)
+        val generateYouTubeThumbnail : Button = findViewById(R.id.generate_thumbnail_for_youtube)
+        img = findViewById(R.id.img)
+
+
+
+
+
+
+
+
+
+        generateYouTubeThumbnail.setOnClickListener{
+            val youtubeLink = youtubeLink.text.toString()
+
+            if(youtubeLink.isNotEmpty()){
+                Previewer.thumbnailFromYoutube(img, youtubeLink)
+            }
+            else{
+                Toast.makeText(this,"Please enter valid Youtube URL",Toast.LENGTH_SHORT).show()
+            }
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
+++ lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
@@ -1,6 +1,17 @@
 package com.cops.iitbhu.previewer.lib
 
 import android.app.Application
+import android.graphics.Bitmap
+import android.graphics.pdf.PdfRenderer
+import android.net.Uri
+import android.os.Environment.DIRECTORY_PICTURES
+import android.os.ParcelFileDescriptor
+import java.io.File
+import java.io.FileOutputStream
+import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import java.net.MalformedURLException
 
 object Previewer {
 
@@ -10,4 +21,68 @@ object Previewer {
         this.appContext = appContext
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //Extracts video ID from Youtube video URL
+    @Throws(MalformedURLException::class)
+    fun extractYoutubeId(url: String): String {
+        val param1: List<String> = url.split("&")
+        val indexOfEqualsTo = param1[0].indexOf('=', 0)
+
+        val videoId = if (indexOfEqualsTo >= 0 && indexOfEqualsTo < param1[0].length) {
+            param1[0].substring(indexOfEqualsTo + 1, param1[0].length)
+        } else
+            param1[0].replace("https://youtu.be/", "")
+        return videoId
+    }
+
+    //Creates thumbnail from Youtube video URL
+    fun thumbnailFromYoutube(imageview: ImageView, url: String) {
+
+        val videoId = extractYoutubeId(url)
+        Log.d("VideoId is->", "" + videoId)
+        val imageURL = "https://img.youtube.com/vi/$videoId/0.jpg"
+
+        Glide.with(appContext).load(imageURL).into(imageview)
+    }
 }
