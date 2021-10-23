2c21be7e5a66b4df50024c622e1046ec985d8279
puranjayK <puranjay01@gmail.com>
2021-10-23 20:35:12 +0530

    Removed unused imports

--- app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
+++ app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
@@ -3,8 +3,6 @@ package com.cops.iitbhu.previewer
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.cops.iitbhu.previewer.databinding.ActivityMainBinding
