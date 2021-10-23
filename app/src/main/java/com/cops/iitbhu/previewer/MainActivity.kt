d9dc725924240769e7705b2cd62c0b4ecbe60e9f
puranjayK <puranjay01@gmail.com>
2021-10-23 18:44:04 +0530

    Added Demo code to get video from storage and show its thumbnail

--- app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
+++ app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.cops.iitbhu.previewer
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.cops.iitbhu.previewer.lib.Previewer
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnVideoThumbnail: Button
+    private lateinit var imgThumbnail: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Previewer.init(application)
+        imgThumbnail = findViewById(R.id.imgThumbnail)
+        btnVideoThumbnail = findViewById(R.id.btnVideoThumbnail)
+        btnVideoThumbnail.setOnClickListener(View.OnClickListener { view ->
+            activityLauncher.launch("video/*")
+        })
     }
+
+    private val activityLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uriVideo: Uri ->
+            Previewer.getVideoThumbnail(uriVideo, imgThumbnail)
+        }
 }
