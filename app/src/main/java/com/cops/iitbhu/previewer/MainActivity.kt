f2a21fe899b05f66e639cef56db205b27ccbb5d3
puranjayK <puranjay01@gmail.com>
2021-10-23 20:33:25 +0530

    Implemented viewBinding

--- app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
+++ app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
@@ -7,23 +7,23 @@ import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.cops.iitbhu.previewer.databinding.ActivityMainBinding
 import com.cops.iitbhu.previewer.lib.Previewer
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnVideoThumbnail: Button
-    private lateinit var imgThumbnail: ImageView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        imgThumbnail = findViewById(R.id.imgThumbnail)
-        btnVideoThumbnail = findViewById(R.id.btnVideoThumbnail)
-        btnVideoThumbnail.setOnClickListener(View.OnClickListener { view ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.btnVideoThumbnail.setOnClickListener(View.OnClickListener { view ->
             activityLauncher.launch("video/*")
         })
     }
 
     private val activityLauncher =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uriVideo: Uri ->
-            Previewer.getVideoThumbnail(uriVideo, imgThumbnail)
+            Previewer.getVideoThumbnail(uriVideo, binding.imgThumbnail)
         }
 }
