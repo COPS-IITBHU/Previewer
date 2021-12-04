305706a544aca76c5af903dd4bcdcc32bca05217
Raina Jain <84968175+RainaJain5@users.noreply.github.com>
2021-12-04 23:14:56 +0530

    Get Youtube video thumbnail
    * Youtube video thumbnail
    
    * Delete gradle.xml
    
    * Delete misc.xml
    
    * Youtube video thumbnail
    
    * Update MainActivity.kt
    
    * Update activity_main.xml
    
    * Update Previewer.kt
    
    * Update strings.xml
    
    * Update build.gradle
    
    * Changes done
    
    * Remove gradle.xml & runConfigurations.xml from changelog
    
    * Changes in comments and tried using URL
    
    * Add suspend function for fetching thumbnail
    
    * Return null instead of throwing exception
    
    Co-authored-by: arpit <arpitshukla62@gmail.com>

--- app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
+++ app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.cops.iitbhu.previewer
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.cops.iitbhu.previewer.databinding.ActivityMainBinding
+import com.cops.iitbhu.previewer.lib.Previewer
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.generateThumbnailForYoutube.setOnClickListener {
+            val youtubeLink = binding.youtubeLink.text.toString()
+
+            if (youtubeLink.isNotEmpty()) {
+                setThumbnail(youtubeLink)
+//                Previewer.setThumbnailFromYouTubeVideoUrl(youtubeLink, binding.img)
+            } else {
+                Toast.makeText(this, "Please enter valid Youtube URL", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun setThumbnail(youtubeLink: String) {
+        lifecycleScope.launchWhenCreated {
+            val bitmap = Previewer.getThumbnailFromVideoUrl(youtubeLink)
+            binding.img.setImageBitmap(bitmap)
+        }
     }
 }
--- lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
+++ lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
@@ -1,6 +1,15 @@
 package com.cops.iitbhu.previewer.lib
 
 import android.app.Application
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.IOException
+import java.net.URL
 
 object Previewer {
 
@@ -10,4 +19,43 @@ object Previewer {
         this.appContext = appContext
     }
 
+    /**
+     * Generates an image ID from the youtube video link
+     * @param youtubeLink
+     * @return Image ID
+     */
+    fun youtubeLinkToImageUrl(youtubeLink: String): String? {
+        val regex =
+            "^((?:https?:)?//)?((?:www|m)\\.)?(youtube\\.com|youtu.be|youtube-nocookie.com)(/(?:[\\w\\-]+\\?v=|feature=|watch\\?|e/|embed/|v/)?)([\\w\\-]+)(\\S+)?\$"
+        return Regex(regex).matchEntire(youtubeLink)?.groupValues?.get(5)
+    }
+
+    /**
+     * Takes youtube video link, generates the thumbnail for the video and set it in the imageview passed
+     * @param youtubeLink Youtube video link
+     * @param imageView   ImageView to display the thumbnail
+     */
+    fun setThumbnailFromYouTubeVideoUrl(youtubeLink: String, imageView: ImageView) {
+
+        val imageId = youtubeLinkToImageUrl(youtubeLink)
+        val imageUrl = "https://img.youtube.com/vi/$imageId/0.jpg"
+
+        Glide.with(imageView.context)
+            .load(imageUrl)
+            .into(imageView)
+    }
+
+    suspend fun getThumbnailFromVideoUrl(youtubeLink: String): Bitmap? {
+        return withContext(Dispatchers.IO) {
+            try {
+                val imageId = youtubeLinkToImageUrl(youtubeLink)
+                val imageUrl = "https://img.youtube.com/vi/$imageId/0.jpg"
+                val url = URL(imageUrl)
+                val bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+                bitmap
+            } catch (e: IOException) {
+                null
+            }
+        }
+    }
 }
