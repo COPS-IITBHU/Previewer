7337534e2bea30b7497e82e427ec9703889aa9d6
Ayushi Gupta <85010904+ayushigupta931@users.noreply.github.com>
2021-12-04 23:35:16 +0530

    Get bitmap from PDF
    * Bitmap of PDF
    
    * Delete deploymentTargetDropDown.xml
    
    * Delete gradle.xml
    
    * Delete misc.xml
    
    * Bitmap of PDF
    
    * Bitmap of PDF
    
    * Updated
    
    * Update runConfigurations.xml
    
    * Update runConfigurations.xml
    
    * Update runConfigurations.xml
    
    * Changes done
    
    * Get Youtube video thumbnail
    
    * Youtube video thumbnail
    
    * Delete gradle.xml
    
    * Delete misc.xml
    
    * Youtube video thumbnail
    
    * Update MainActivity.kt
    
    * Update activity_main.xml
    
    * Update Previewer.kt
    
    * Update strings.xml
    
    * Update build.gradle
    
    * Changes done
    
    * Remove gradle.xml & runConfigurations.xml from changelog
    
    * Changes in comments and tried using URL
    
    * Add suspend function for fetching thumbnail
    
    * Return null instead of throwing exception
    
    Co-authored-by: arpit <arpitshukla62@gmail.com>
    
    * Reformat files
    
    Co-authored-by: Raina Jain <84968175+RainaJain5@users.noreply.github.com>
    Co-authored-by: arpit <arpitshukla62@gmail.com>

--- app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
+++ app/src/main/java/com/cops/iitbhu/previewer/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.cops.iitbhu.previewer
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.cops.iitbhu.previewer.databinding.ActivityMainBinding
@@ -11,11 +13,26 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val getUriOfPdf =
+        registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+
+            if (uri != null) {
+                val bitmap = Previewer.generateBitmapFromPdf(uri)
+                if (bitmap != null)
+                    binding.img.setImageBitmap(bitmap)
+                else
+                    Toast.makeText(this, "Unable to generate bitmap!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
+
+        binding.selectFile.setOnClickListener(View.OnClickListener {
+            getUriOfPdf.launch(arrayOf("application/pdf"))
+        })
 
         binding.generateThumbnailForYoutube.setOnClickListener {
             val youtubeLink = binding.youtubeLink.text.toString()
