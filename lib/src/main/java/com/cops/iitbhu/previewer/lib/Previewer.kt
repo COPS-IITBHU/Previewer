bd3740bc566ca3be1c86e319d8306c10e31b4b09
arpit <arpitshukla62@gmail.com>
2021-12-04 23:25:20 +0530

    Reformat files

--- lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
+++ lib/src/main/java/com/cops/iitbhu/previewer/lib/Previewer.kt
@@ -3,13 +3,19 @@ package com.cops.iitbhu.previewer.lib
 import android.app.Application
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.util.Log
+import android.graphics.pdf.PdfRenderer
+import android.net.Uri
+import android.os.Environment.DIRECTORY_DOCUMENTS
+import android.os.ParcelFileDescriptor
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
 import java.net.URL
+import kotlin.math.min
 
 object Previewer {
 
@@ -56,6 +62,50 @@ object Previewer {
             } catch (e: IOException) {
                 null
             }
+        }
+    }
+
+    //Generates bitmap for pdf
+    fun generateBitmapFromPdf(uri: Uri): Bitmap? {
+
+        try {
+            val fileName = "@${System.currentTimeMillis()}"
+            val storageDir = appContext.getExternalFilesDir(DIRECTORY_DOCUMENTS)
+            val file = File.createTempFile(fileName, ".pdf", storageDir!!)
+            val inputStream = appContext.contentResolver.openInputStream(uri)!!
+            val outputStream = FileOutputStream(file)
+            val buffer = ByteArray(1024)
+            var len: Int
+
+            while (inputStream.read(buffer).also { len = it } >= 0) {
+                outputStream.write(buffer, 0, len)
+            }
+
+            outputStream.close()
+            inputStream.close()
+
+            val fileDescriptor =
+                ParcelFileDescriptor.open(file, ParcelFileDescriptor.MODE_READ_ONLY)
+            val pdfRenderer = PdfRenderer(fileDescriptor)
+            val page = pdfRenderer.openPage(0)
+            var bitmap: Bitmap = Bitmap.createBitmap(
+                page.width,
+                page.height,
+                Bitmap.Config.ARGB_8888
+            )
+
+            page.render(bitmap, null, null, PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY)
+            val size = min(page.height, page.width)
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, size, size)
+
+            file.delete()
+            page.close()
+            pdfRenderer.close()
+            fileDescriptor.close()
+            return bitmap
+        } catch (e: Exception) {
+            println(e)
+            return null
         }
     }
 }
