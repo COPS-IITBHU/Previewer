7174b8be3172110d1876bdcabaa8b61f19e52bbf
mihir02badkur <mihir27badkur@gmail.com>
2021-12-05 16:57:01 +0530

    Regex tests for Youtube URLs

--- lib/src/test/java/com/cops/iitbhu/previewer/lib/RegexTest.kt
+++ lib/src/test/java/com/cops/iitbhu/previewer/lib/RegexTest.kt
@@ -8,9 +8,47 @@ class RegexTest {
 
     @Test
     fun checkRegexIsValid() {
-        val youtubeLink = "https://www.youtube.com/watch?v=Pur_0KroFR8"
-        val expected = "Pur_0KroFR8"
-        val actual = Previewer.youtubeLinkToImageUrl(youtubeLink)
-        assertEquals(expected, actual)
+        val links= listOf(
+            "https://www.youtube.com/watch?v=DFYRQ_zQ-gk",
+            "https://www.youtube.com/watch?v=DFYRQ_zQ-gk&feature=featured",
+            "https://www.youtube.com/watch?v=DFYRQ_zQ-gk",
+            "http://www.youtube.com/watch?v=DFYRQ_zQ-gk",
+            "//www.youtube.com/watch?v=DFYRQ_zQ-gk",
+            "www.youtube.com/watch?v=DFYRQ_zQ-gk",
+            "https://youtube.com/watch?v=DFYRQ_zQ-gk",
+            "http://youtube.com/watch?v=DFYRQ_zQ-gk",
+            "//youtube.com/watch?v=DFYRQ_zQ-gk",
+            "youtube.com/watch?v=DFYRQ_zQ-gk",
+            "https://m.youtube.com/watch?v=DFYRQ_zQ-gk",
+            "http://m.youtube.com/watch?v=DFYRQ_zQ-gk",
+            "//m.youtube.com/watch?v=DFYRQ_zQ-gk",
+            "m.youtube.com/watch?v=DFYRQ_zQ-gk",
+            "https://www.youtube.com/v/DFYRQ_zQ-gk?fs=1&hl=en_US",
+            "http://www.youtube.com/v/DFYRQ_zQ-gk?fs=1&hl=en_US",
+            "//www.youtube.com/v/DFYRQ_zQ-gk?fs=1&hl=en_US",
+            "www.youtube.com/v/DFYRQ_zQ-gk?fs=1&hl=en_US",
+            "youtube.com/v/DFYRQ_zQ-gk?fs=1&hl=en_US",
+            "https://www.youtube.com/embed/DFYRQ_zQ-gk?autoplay=1",
+            "https://www.youtube.com/embed/DFYRQ_zQ-gk",
+            "http://www.youtube.com/embed/DFYRQ_zQ-gk",
+            "//www.youtube.com/embed/DFYRQ_zQ-gk",
+            "www.youtube.com/embed/DFYRQ_zQ-gk",
+            "https://youtube.com/embed/DFYRQ_zQ-gk",
+            "http://youtube.com/embed/DFYRQ_zQ-gk",
+            "//youtube.com/embed/DFYRQ_zQ-gk",
+            "youtube.com/embed/DFYRQ_zQ-gk",
+            "https://youtu.be/DFYRQ_zQ-gk?t=120",
+            "https://youtu.be/DFYRQ_zQ-gk",
+            "http://youtu.be/DFYRQ_zQ-gk",
+            "//youtu.be/DFYRQ_zQ-gk",
+            "youtu.be/DFYRQ_zQ-gk",
+            "https://www.youtube.com/HamdiKickProduction?v=DFYRQ_zQ-gk",
+
+        )
+        val expected = "DFYRQ_zQ-gk"
+        links.forEach{ link->
+            val actual = Previewer.youtubeLinkToImageUrl(link)
+            assertEquals(expected, actual)
+        }
     }
 }
