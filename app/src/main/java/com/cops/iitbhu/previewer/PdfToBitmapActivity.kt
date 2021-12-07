d80876381a7c3e8b2571e18c3b6bf7a005452a3e
Mihir Badkur <85284909+mihir02badkur@users.noreply.github.com>
2021-12-07 16:54:34 +0530

    Create separate activity for each functionality
    * Get bitmap from PDF
    
    * Bitmap of PDF
    
    * Delete deploymentTargetDropDown.xml
    
    * Delete gradle.xml
    
    * Delete misc.xml
    
    * Bitmap of PDF
    
    * Bitmap of PDF
    
    * Updated
    
    * Update runConfigurations.xml
    
    * Update runConfigurations.xml
    
    * Update runConfigurations.xml
    
    * Changes done
    
    * Get Youtube video thumbnail
    
    * Youtube video thumbnail
    
    * Delete gradle.xml
    
    * Delete misc.xml
    
    * Youtube video thumbnail
    
    * Update MainActivity.kt
    
    * Update activity_main.xml
    
    * Update Previewer.kt
    
    * Update strings.xml
    
    * Update build.gradle
    
    * Changes done
    
    * Remove gradle.xml & runConfigurations.xml from changelog
    
    * Changes in comments and tried using URL
    
    * Add suspend function for fetching thumbnail
    
    * Return null instead of throwing exception
    
    Co-authored-by: arpit <arpitshukla62@gmail.com>
    
    * Reformat files
    
    Co-authored-by: Raina Jain <84968175+RainaJain5@users.noreply.github.com>
    Co-authored-by: arpit <arpitshukla62@gmail.com>
    
    * Add support for generating video thumbnail
    
    * Added glide dependency
    
    * Added support for generating video thumbnail
    
    * added necessary imports
    
    * Bitmap of PDF
    
    * Delete deploymentTargetDropDown.xml
    
    * Delete gradle.xml
    
    * Delete misc.xml
    
    * Bitmap of PDF
    
    * Bitmap of PDF
    
    * Updated
    
    * Update runConfigurations.xml
    
    * Update runConfigurations.xml
    
    * Update runConfigurations.xml
    
    * Changes done
    
    * Added Demo code to get video from storage and show its thumbnail
    
    * removed blank line
    
    * removed .asBitmap()
    
    * removed Previewer.init() call from MainActivity.kt
    
    * Implemented viewBinding
    
    * Removed unused imports
    
    * renamed getVideoThumbnail function to setVideoThumbnailFromUri
    
    * renamed activityLauncher to activityResultLauncherForVideoUri
    
    * added code to demonstrate creation of thumbnail from Internet Video
    
    * added empty line after declaration
    
    * renamed uriVideo to videoUri
    
    * Removed redundant code
    
    * Added internet permission
    
    * Fixed the typo
    
    * Reformat files
    
    Co-authored-by: ayushigupta931 <ayushigupta931@gmail.com>
    Co-authored-by: Ayushi Gupta <85010904+ayushigupta931@users.noreply.github.com>
    Co-authored-by: arpit <arpitshukla62@gmail.com>
    
    * Create separate activities for all functions
    
    * change image view size
    
    * apply suggested changes
    
    * Apply suggestions
    
    Co-authored-by: Ayushi Gupta <85010904+ayushigupta931@users.noreply.github.com>
    Co-authored-by: Raina Jain <84968175+RainaJain5@users.noreply.github.com>
    Co-authored-by: arpit <arpitshukla62@gmail.com>
    Co-authored-by: Puranjay Khanijo <85157266+puranjayK@users.noreply.github.com>
    Co-authored-by: ayushigupta931 <ayushigupta931@gmail.com>

---
+++ app/src/main/java/com/cops/iitbhu/previewer/PdfToBitmapActivity.kt
@@ -0,0 +1,34 @@
+package com.cops.iitbhu.previewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.cops.iitbhu.previewer.databinding.ActivityPdfToBitmapBinding
+import com.cops.iitbhu.previewer.lib.Previewer
+
+class PdfToBitmapActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPdfToBitmapBinding
+
+    private val getUriOfPdf =
+        registerForActivityResult(ActivityResultContracts.OpenDocument()) { uri ->
+            if (uri != null) {
+                val bitmap = Previewer.generateBitmapFromPdf(uri)
+                if (bitmap != null)
+                    binding.image.setImageBitmap(bitmap)
+                else
+                    Toast.makeText(this, "Unable to generate bitmap!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPdfToBitmapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.selectFile.setOnClickListener {
+            getUriOfPdf.launch(arrayOf("application/pdf"))
+        }
+    }
+}
